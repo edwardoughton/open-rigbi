fbc47f03a48a16bd13d7e55b06b2c98baeab1ca9
edwardoughton <edward.oughton@gmail.com>
2022-02-15 18:33:27 -0500

    Add flood hazard processing script

---
+++ scripts/flood_hazards.py
@@ -0,0 +1,131 @@
+"""
+Preprocess sites data.
+
+Ed Oughton
+
+February 2022
+
+"""
+import os
+import json
+import configparser
+import pandas as pd
+import geopandas as gpd
+import rasterio
+from rasterio.mask import mask
+from rasterstats import zonal_stats
+
+CONFIG = configparser.ConfigParser()
+CONFIG.read(os.path.join(os.path.dirname(__file__), 'script_config.ini'))
+BASE_PATH = CONFIG['file_locations']['base_path']
+
+DATA_RAW = os.path.join(BASE_PATH, 'raw')
+DATA_PROCESSED = os.path.join(BASE_PATH, 'processed')
+
+filename = "countries.csv"
+path = os.path.join(DATA_RAW, filename)
+countries = pd.read_csv(path, encoding='latin-1')
+
+
+def process_hazard_layer(country, path_in, path_out):
+    """
+    Clip the hazard layer to the chosen country boundary
+    and place in desired country folder.
+
+    Parameters
+    ----------
+    country : dict
+        Contains all desired country information.
+    path_in : string
+        The path for the chosen global hazard file to be processed.
+    path_out : string
+        The path to write the clipped hazard file.
+
+    """
+    iso3 = country['iso3']
+    regional_level = country['gid_region']
+
+    hazard = rasterio.open(path_in, 'r+')
+    hazard.nodata = 255
+    hazard.crs.from_epsg(4326)
+
+    iso3 = country['iso3']
+    path_country = os.path.join(DATA_PROCESSED, iso3,
+        'national_outline.shp')
+
+    if os.path.exists(path_country):
+        country = gpd.read_file(path_country)
+    else:
+        print('Must generate national_outline.shp first' )
+
+    if os.path.exists(path_out):
+        return
+
+    bbox = country.envelope
+    geo = gpd.GeoDataFrame()
+
+    geo = gpd.GeoDataFrame({'geometry': bbox})
+
+    coords = [json.loads(geo.to_json())['features'][0]['geometry']]
+
+    out_img, out_transform = mask(hazard, coords, crop=True)
+
+    out_meta = hazard.meta.copy()
+
+    out_meta.update({"driver": "GTiff",
+                    "height": out_img.shape[1],
+                    "width": out_img.shape[2],
+                    "transform": out_transform,
+                    "crs": 'epsg:4326'})
+
+    with rasterio.open(path_out, "w", **out_meta) as dest:
+            dest.write(out_img)
+
+    return
+
+if __name__ == "__main__":
+
+    os.environ['GDAL_DATA'] = ("C:\\Users\\edwar\\Anaconda3\\Library\\share\\gdal")
+
+    failures = []
+
+    for idx, country in countries.iterrows():
+
+        iso3 = country['iso3']
+        name = country['country']
+
+        # if not iso3 == 'ATA':
+        #     continue
+
+        filenames = [
+            'inunriver_historical_000000000WATCH_1980_rp01000.tif',
+            'inuncoast_historical_nosub_hist_rp1000_0.tif',
+            'inuncoast_historical_wtsub_2080_rp1000_0.tif',
+            'inuncoast_rcp4p5_wtsub_2080_rp1000_0_perc_50.tif',
+            'inuncoast_rcp8p5_wtsub_2080_rp1000_0_perc_50.tif',
+        ]
+
+        for filename in filenames:
+
+            folder = os.path.join(DATA_RAW, 'hazard_scenarios')
+            path_in = os.path.join(folder, filename)
+
+            folder = os.path.join(DATA_PROCESSED, iso3, 'hazards')
+            path_out = os.path.join(folder, filename)
+
+            if not os.path.exists(path_out):
+
+                print('--{}: {}'.format(name, filename))
+
+                if not os.path.exists(folder):
+                    os.makedirs(folder)
+
+                try:
+                    outcome = process_hazard_layer(country, path_in, path_out)
+                except:
+                    failures.append({
+                        'iso3': iso3,
+                        'filename': filename
+                    })
+
+    print(failures)
