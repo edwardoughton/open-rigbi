50d0e19fa0d4a4e06fcac0ff532a8f184b527c8d
edwardoughton <edward.oughton@gmail.com>
2022-04-13 13:54:46 -0400

    Edit path

--- scripts/dl.py
+++ scripts/dl.py
@@ -3,10 +3,17 @@ Download Aquaduct data.
 
 """
 import os
+import configparser
 from lxml import html
 import requests
 import urllib.request
 from tqdm import tqdm
+
+CONFIG = configparser.ConfigParser()
+CONFIG.read(os.path.join(os.path.dirname(__file__), 'script_config.ini'))
+BASE_PATH = CONFIG['file_locations']['base_path']
+
+DATA_RAW = os.path.join(BASE_PATH, 'raw')
 
 path = 'http://wri-projects.s3.amazonaws.com/AqueductFloodTool/download/v2/index.html'
 page = requests.get(path)
@@ -17,12 +24,15 @@ for in_path in tqdm(webpage.xpath('//a/@href')):
     if not in_path.endswith('.tif'):
         continue
 
-    # if 'wtsub' in in_path:
-    #     continue
-
     filename = os.path.basename(in_path)
 
-    out_path = "data/raw/hazard_scenarios/{}".format(filename)
+    folder = os.path.join(DATA_RAW, 'hazard_scenarios')
+
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+
+    filename = "{}".format(filename)
+    out_path = os.path.join(folder, filename)
 
     if not os.path.exists(out_path):
 
