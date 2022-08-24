c12fb3c3c56a1981f7d9467138bc4780f3b77c01
EdwardOughton <edward.oughton@gmail.com>
2022-08-24 17:56:07 -0400

    Add run parallel

--- parallel/run_parallel.sh
+++ parallel/run_parallel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-countries=`python parallel/input_generator.py`
+countries=`python input_generator.py`
 
 # echo $countries
 
-echo `pwd`/scripts/network_preprocess_input_files.sh $countries
+# echo `pwd`/scripts/network_preprocess_input_files.sh $countries
 
 #seq 1 $count | parallel -n1 --no-notice ./plot_stations.py {}
-parallel --sshloginfile nodeslist -n1 --no-notice --progress `pwd`/scripts/network_preprocess_input_files.sh ::: $countries
+parallel --sshloginfile nodeslist.txt -n1 --no-notice --progress `pwd`/scripts/run_country.sh ::: $countries
 
 echo 'parallel processing complete'
