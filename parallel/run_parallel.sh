fdaa670ea6024c753ca02d40cc68ad416e350103
root <root@edward.novalocal>
2022-09-06 16:00:41 -0400

    add_updates

--- parallel/run_parallel.sh
+++ parallel/run_parallel.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-# source activate rigbi-env
 
 countries=`python /projects/open-rigbi/parallel/input_generator.py`
 
 echo $countries
 
-# echo `pwd`/scripts/network_preprocess_input_files.sh $countries
 #seq 1 $count | parallel -n1 --no-notice ./plot_stations.py {}
 parallel --sshloginfile /projects/open-rigbi/parallel/nodelist -n1 --no-notice --progress /projects/open-rigbi/parallel/run_country.sh ::: $countries
 
