02d26fb767efbbc4e6c558128f066697e53ad749
edwardoughton <edward.oughton@gmail.com>
2022-10-09 08:57:50 -0400

    update processing for large countries

--- parallel/run_country.sh
+++ parallel/run_country.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 # . /etc/profile.d/profile.sh
-echo $1: Running on `hostname`
+echo -----running on `hostname`: $1
 
 DIR=/projects/open-rigbi
-#DIR=$(pwd)
-
-# echo $DIR
 
 source activate rigbi-env
-#cd D:/github/open-rigbi/
-cd $DIR
 
-#cd /soge-home/projects/mistral/nismod/digital_comms
-#mkdir -p D:/github/open-rigbi/data/test/$1
 mkdir -p $DIR/data/processed/$1
 
-#touch D:/github/open-rigbi/data/test/$1/log.txt
 touch $DIR/data/processed/$1/log.txt
 
 # python D:/github/open-rigbi/parallel/input_generator.py $1
 # # D:\Github\open-rigbi\parallel\input_generator.py
 # echo python `pwd`/parallel/sites.py
 python $DIR/scripts/process.py $1 &> $DIR/data/processed/$1/log.txt
+
+echo --completed: $1
