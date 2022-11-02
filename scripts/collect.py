2c2c374e39a5d666448b54903a4994b1f1a14fe0
EdwardOughton <edward.oughton@gmail.com>
2022-11-02 09:09:02 -0400

    Add final results collection funcs.

--- scripts/collect.py
+++ scripts/collect.py
@@ -146,15 +146,154 @@ def collect_country_regional_results(iso3, scenario):
     return
 
 
+
+def collect_final_results(scenario):
+    """
+    Collect all results.
+
+    """
+    countries = get_countries()
+
+    folder_out = os.path.join(DATA_PROCESSED, 'results')
+    if not os.path.exists(folder_out):
+        os.mkdir(folder_out)
+
+    output = []
+
+    scenario_name = os.path.basename(scenario)[:-4]
+
+    path_out = os.path.join(folder_out, scenario_name + '.csv')
+
+    for idx, country in countries.iterrows():
+
+        # if not collection_type == 'all':
+        #     if not country['iso3'] == collection_type:
+        #         continue
+
+        collect_national_results(country['iso3'], scenario)
+
+        path = os.path.join(DATA_PROCESSED, country['iso3'], 'results',
+            'national_data', scenario_name + '.csv')
+
+        if not os.path.exists(path):
+            output.append({
+                    'iso3': country['iso3'],
+                    'iso2': country['iso2'],
+                    'country': country['country'],
+                    'continent': country['continent'],
+                    'radio': 'NA',
+                    'network': 'NA',
+                    'cell_count': 0,
+                    'cost_usd': 0,
+                })
+            continue
+
+        # print(path)
+        data = pd.read_csv(path, sep=',')
+        # print(data.columns)
+        if len(data) == 0:
+            continue
+
+        radios = list(data['radio'].unique())
+        networks = list(data['net'].unique())
+
+        for radio in radios:
+
+            #for network in networks:
+
+            cell_count = 0
+            cost_usd = 0
+
+            for idx, item in data.iterrows():
+
+                if not item['radio'] == radio:
+                    continue
+
+            #        if not item['net'] == network:
+            #            continue
+
+                if not item['failure'] == 1:
+                    continue
+                # print(cell_count)
+                cell_count += 1
+                cost_usd += item['cost_usd']
+
+            output.append({
+                'iso3': country['iso3'],
+                'iso2': country['iso2'],
+                'country': country['country'],
+                'continent': country['continent'],
+                'radio': radio,
+                #'network': network,
+                'cell_count': cell_count,
+                'cost_usd': cost_usd,
+                })
+
+    if len(output) == 0:
+        # print('output len = 0')
+        return
+
+    output = pd.DataFrame(output)
+
+    output.to_csv(path_out, index=False)
+
+    return
+
+
+def collect_national_results(iso3, scenario):
+    """
+    Collect regional results and write to national results folder.
+
+    """
+    output = []
+
+    scenario_name = os.path.basename(scenario)[:-4]
+    #print('collecting national results for {}'.format(scenario_name))
+    folder = os.path.join(DATA_PROCESSED, iso3, 'results', 'regional_data', scenario_name)
+
+    if not os.path.exists(folder):
+        #print('collect_national_results: folder does not exist: {}'.format(folder))
+        return
+
+    all_regional_results = os.listdir(folder)
+
+    if len(all_regional_results) == 0:
+        #print('len of all_regional_results = 0')
+        return
+
+    for filename in all_regional_results:
+
+        path_in = os.path.join(folder, filename)
+
+        if not os.path.exists(path_in):
+            continue
+        try:
+            data = pd.read_csv(path_in)
+            data = data.to_dict('records')
+            output = output + data
+        except:
+            print('failed on {})'.format(path_in))
+
+    if len(output) == 0:
+        #print('2. len of output = 0')
+        return
+
+    output = pd.DataFrame(output)
+
+    folder_out = os.path.join(DATA_PROCESSED, iso3, 'results', 'national_data')
+    if not os.path.exists(folder_out):
+        print('folder out did not exist')
+        os.mkdir(folder_out)
+    path_out = os.path.join(folder_out, scenario_name + '.csv')
+    output.to_csv(path_out, index=False)
+
+    return
+
+
 if __name__ == "__main__":
 
     args = sys.argv
 
     collect_regional_results(args[1])
 
-    # if len(args[1]) > 0:
-    #     # collect_final_results(args[2])
-    #     collect_regional_results(args[1])
-    # else:
-    #     # collect_final_results('all')
-    #     collect_regional_results('all')
+    collect_final_results(args[1])
