212d8ba6bdae42b4fc98679be390012ee818eb5c
EdwardOughton <edward.oughton@gmail.com>
2022-12-18 14:27:55 -0500

    Add vis updates

--- vis/inunriver.R
+++ vis/inunriver.R
@@ -7,7 +7,6 @@ install.packages("llply")
 require(llply)
 library(stringr)
 
-
 ###################
 #####Aggregate cells
 folder = dirname(rstudioapi::getSourceEditorContext()$path)
@@ -51,63 +50,289 @@ data = data[
     data$climatescenario == 'rcp4p5' | 
     data$climatescenario == "rcp8p5",]
 
-data$returnperiod =  gsub(".csv", "", data$returnperiod) 
-data$returnperiod =  gsub("rp0", "rp", data$returnperiod) 
-
-data  = data %>% 
-  group_by(climatescenario, subsidence_model, year, 
-           returnperiod, radio) %>% 
-  summarise(cell_count = sum(cell_count))
-
-data$interaction = paste(data$year, data$climatescenario)
-
-data$interaction = factor(data$interaction,
-                          levels=c(
-                            "1980 historical", 
-                            "hist historical",
-                            "2030 rcp4p5",
-                            "2050 rcp4p5",
-                            "2080 rcp4p5",
-                            "2030 rcp8p5",
-                            "2050 rcp8p5",
-                            "2080 rcp8p5"),
-                          labels=c("Historical",
-                                   "Historical",
-                                   "2030 RCP4.5",
-                                   "2030 RCP8.5",
-                                   "2050 RCP4.5",
-                                   "2050 RCP8.5",
-                                   "2080 RCP4.5",
-                                   "2080 RCP8.5")
+data$climatescenario = factor(data$climatescenario,
+                    levels=c("historical","rcp4p5","rcp8p5"),
+                    labels=c("Historical","RCP4.5","RCP8.5")
 )
 
+data$returnperiod =  gsub(".csv", "", data$returnperiod) 
+
+#replace return periods
+data$returnperiod =  gsub("rp00002", "rp0002", data$returnperiod)
+data$returnperiod =  gsub("rp00005", "rp0005", data$returnperiod)
+data$returnperiod =  gsub("rp00010", "rp0010", data$returnperiod)
+data$returnperiod =  gsub("rp00025", "rp0025", data$returnperiod)
+data$returnperiod =  gsub("rp00050", "rp0050", data$returnperiod)
+data$returnperiod =  gsub("rp00100", "rp0100", data$returnperiod)
+data$returnperiod =  gsub("rp00250", "rp0250", data$returnperiod)
+data$returnperiod =  gsub("rp00500", "rp0500", data$returnperiod)
+data$returnperiod =  gsub("rp01000", "rp1000", data$returnperiod)
+
+#convert to probability_of_exceedance 
+data$probability = ''
+data$probability[data$returnperiod == "rp0002"] = "50%" # (1/2) * 100 = 50%
+data$probability[data$returnperiod == "rp0005"] = "20%" # (1/10) * 100 = 10%
+data$probability[data$returnperiod == "rp0010"] = "10%" # (1/10) * 100 = 10%
+data$probability[data$returnperiod == "rp0025"] = "4%" # (1/25) * 100 = 4%
+data$probability[data$returnperiod == "rp0050"] = "2%" # (1/50) * 100 = 2%
+data$probability[data$returnperiod == "rp0100"] = "1%" # (1/100) * 100 = 1%
+data$probability[data$returnperiod == "rp0250"] = "0.4%" # (1/250) * 100 = .4%
+data$probability[data$returnperiod == "rp0500"] = "0.2%" # (1/500) * 100 = .2%
+data$probability[data$returnperiod == "rp1000"] = "0.1%" # (1/1000) * 100 = .1%
+
 data$returnperiod = factor(data$returnperiod,
+                          levels=c(
+                            "rp0002",
+                            "rp0005",
+                            "rp0010",
+                            "rp0025",
+                            "rp0050",
+                            "rp0100",
+                            "rp0250",
+                            "rp0500",
+                            "rp1000"
+                          ),
+                          labels=c(
+                            "1-in-2-Years",
+                            "1-in-5-Years",
+                            "1-in-10-Years",
+                            "1-in-25-Years",
+                            "1-in-50-Years",
+                            "1-in-100-Years",
+                            "1-in-250-Years",
+                            "1-in-500-Years",
+                            "1-in-1000-Years"
+                          )
+)
+
+data$probability = factor(data$probability,
                            levels=c(
-                             # "100-year",
-                             # "250-year",
-                             # "500-year",
-                             "rp1000"
-                           ),
-                           labels=c(
-                             # "100-year",
-                             # "250-year",
-                             # "500-year",
-                             "1000-Year Return Period"
+                             "0.1%",
+                             "0.2%",
+                             "0.4%",
+                             "1%",
+                             "2%",
+                             "4%",
+                             "10%",
+                             "20%",
+                             "50%"
                            )
 )
 
+data = data[data$probability == "0.1%"  |
+              data$probability == "0.2%"  |
+              data$probability == "0.4%"  |
+              data$probability == "1%", ] 
+
 data$radio = factor(data$radio,
-                    # levels=c("NR","LTE","UMTS",
-                    #          "GSM"),
                     levels=c("GSM","UMTS",
                              "LTE","NR"),
-                    # labels=c("5G NR","4G LTE",
-                    #          "3G UMTS","2G GSM"
-                    # )
                     labels=c("2G GSM","3G UMTS",
                              "4G LTE","5G NR"
                     )
 )
+
+data = data[data$subsidence_model != 'model-mean', ] 
+
+data$subsidence_model = factor(data$subsidence_model,
+                   levels=c("wtsub",
+                            "000000000WATCH",
+                            "00000NorESM1-M",
+                            "0000GFDL-ESM2M",
+                            "0000HadGEM2-ES",
+                            "00IPSL-CM5A-LR",
+                            "MIROC-ESM-CHEM"),
+                   labels=c("RISES-AM",
+                            "Historical Baseline",
+                            "00000NorESM1-M",
+                            "0000GFDL-ESM2M",
+                            "0000HadGEM2-ES",
+                            "00IPSL-CM5A-LR",
+                            "MIROC-ESM-CHEM"
+                   )
+)
+
+inunriver = data[data$hazard_type == 'inunriver',]
+
+inunriver  = inunriver %>% 
+  group_by(climatescenario, subsidence_model, year, 
+           probability, returnperiod) %>% 
+  summarise(cell_count = round(sum(cell_count)/1e6,2))
+
+historical = inunriver[inunriver$year == '1980',]
+
+hist_2030 = historical
+hist_2030$year = "2030"
+hist_2050 = historical
+hist_2050$year = "2050"
+hist_2080 = historical
+hist_2080$year = "2080"
+
+historical = rbind(hist_2030, hist_2050, hist_2080)
+
+results = inunriver[inunriver$year != '1980',]
+
+results = rbind(results, historical)
+
+results_summary <- results %>%
+  group_by(climatescenario, year, probability, returnperiod) %>%
+  summarise(
+    sd = sd(cell_count, na.rm = TRUE),
+    cell_count = mean(cell_count)
+  )
+
+
+rm(historical, hist_2030, hist_2050, hist_2080)
+
+max_y_value = max(inunriver$cell_count)
+
+
+ggplot(results, aes(probability, cell_count, color = climatescenario)) +
+  # geom_jitter(position = position_jitter(0.2)) + 
+  geom_line(aes(group = climatescenario), data = results_summary) +
+  geom_errorbar(aes(ymin = cell_count-sd, ymax = cell_count+sd), 
+                data = results_summary, width = 0.2)+
+  # scale_color_manual(values = c("#00AFBB", "#E7B800", "#E7B800")) +
+  theme(legend.position = "bottom",
+        axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1, size=8),
+        # text = element_text(size=8),
+  ) +
+  labs(colour=NULL,
+       title = "Estimated Coastal Flooding Impact to Cellular Voice/Data Cells",
+       subtitle = "Reported by Annual Probability, Year, and Climate Scenario", 
+       x = "Probability", y = "Cells (Millions)", fill=NULL) +
+  # scale_color_manual(values=c("#0072B2", "#D55E00")) +
+  theme(panel.spacing = unit(0.6, "lines")) + 
+  expand_limits(y=0) +
+  guides(linetype=guide_legend(ncol=3, title='Scenario'),
+         shape=guide_legend(ncol=3, title='Scenario'),
+         color=guide_legend(ncol=3, title='Scenario')) +
+  scale_fill_viridis_d(direction=1) +
+  scale_x_discrete(expand = c(0, 0.15)) +
+  scale_y_continuous(expand = c(0, 0), limits=c(0, max_y_value+.02)) +
+  facet_wrap(~year)
+
+ggplot(results_summary, aes(probability, cell_count)) +
+  geom_errorbar(
+    aes(ymin = cell_count-sd, ymax = cell_count+sd, color = climatescenario),
+    position = position_dodge(0.3), width = 0.2
+  )+
+  geom_point(aes(color = climatescenario), position = position_dodge(0.3)) +
+  scale_color_manual(values = c("#00AFBB", "#E7B800", "#E7B800")) +  
+  facet_wrap(~year)
+
+
+ggplot(results_summary, aes(x=probability, y=cell_count, 
+                      color=climatescenario, shape = subsidence_model
+)
+) +
+  geom_point(position = position_dodge(width = .3)) +  
+  facet_wrap(~year)
+
+
+
+
+
+
+inunriver = rbind(inunriver, hist_2030)
+inunriver = rbind(inunriver, hist_2050)
+inunriver = rbind(inunriver, hist_2080)
+
+
+
+
+
+
+
+
+
+
+
+inunriver$year = factor(inunriver$year,
+                   levels=c("2030", "2050","2080"),
+)
+
+
+
+
+
+
+
+path = file.path(folder, 'figures', '1a_cell_count_aggregate_cell_damage.png')
+ggsave(path, units="in", width=6, height=5, dpi=300)
+
+
+
+
+
+
+
+
+
+
+
+inuncoast = data[data$hazard_type == 'inuncoast',]
+
+inuncoast  = inuncoast %>% 
+  group_by(climatescenario, subsidence_model, year, 
+           probability, returnperiod) %>% 
+  summarise(cell_count = round(sum(cell_count)/1e6,2))
+
+max_y_value = max(inuncoast$cell_count)
+
+ggplot(inuncoast, aes(x=probability, y=cell_count, 
+                    group=climatescenario, color=climatescenario)) +
+  geom_line() +  
+  theme(legend.position = "bottom",
+        axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1, size=8),
+         # text = element_text(size=8),
+  ) +
+  labs(colour=NULL,
+       title = "Estimated Coastal Flooding Impact to Cellular Voice/Data Cells",
+       subtitle = "Reported by Annual Probability, Year, and Climate Scenario", 
+       x = "Probability", y = "Cells (Millions)", fill=NULL) +
+  # scale_color_manual(values=c("#0072B2", "#D55E00")) +
+  theme(panel.spacing = unit(0.6, "lines")) + 
+  expand_limits(y=0) +
+  guides(linetype=guide_legend(ncol=3, title='Scenario'),
+         shape=guide_legend(ncol=3, title='Scenario'),
+         color=guide_legend(ncol=3, title='Scenario')) +
+  scale_fill_viridis_d(direction=1) +
+  scale_x_discrete(expand = c(0, 0.15)) +
+  scale_y_continuous(expand = c(0, 0), limits=c(0, max_y_value+.02)) +
+  facet_wrap(~year)
+
+path = file.path(folder, 'figures', '1a_cell_count_aggregate_cell_damage.png')
+ggsave(path, units="in", width=6, height=5, dpi=300)
+
+
+# coastal$interaction = paste(coastal$year, coastal$climatescenario)
+# unique(coastal$interaction)
+# coastal$interaction = factor(coastal$interaction,
+#                           levels=c(
+#                             "hist historical",
+#                             "2030 historical", 
+#                             "2050 historical",
+#                             "2080 historical",
+#                             "2030 rcp4p5",
+#                             "2050 rcp4p5",
+#                             "2080 rcp4p5",
+#                             "2030 rcp8p5",
+#                             "2050 rcp8p5",
+#                             "2080 rcp8p5"),
+#                           labels=c("1980 Historical",
+#                                    "2030 Historical",
+#                                    "2050 Historical",
+#                                    "2080 Historical",
+#                                    "2030 RCP4.5",
+#                                    "2030 RCP8.5",
+#                                    "2050 RCP4.5",
+#                                    "2050 RCP8.5",
+#                                    "2080 RCP4.5",
+#                                    "2080 RCP8.5")
+# )
+
+
 
 data$cell_count = (data$cell_count/1e6)
 
