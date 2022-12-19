0fa796be9b731f3e96cd7dff4f2bfaf4bfdc09c5
EdwardOughton <edward.oughton@gmail.com>
2022-12-19 12:16:56 -0500

    Add rio merge

--- scripts/flood_hazards.py
+++ scripts/flood_hazards.py
@@ -132,6 +132,8 @@ def process_surface_water_layers(country):
     folder = os.path.join(DATA_RAW, 'global_surface_water')
     paths = glob.glob(os.path.join(folder, "*.tif"))#[:5]
 
+    failures = []
+
     for path_in in paths:
 
         filename = os.path.basename(path_in)
@@ -139,8 +141,6 @@ def process_surface_water_layers(country):
         path_out = os.path.join(folder, filename)
 
         if not os.path.exists(path_out):
-
-            print('--{}: {}'.format(name, filename))
 
             if not os.path.exists(folder):
                 os.makedirs(folder)
@@ -152,6 +152,8 @@ def process_surface_water_layers(country):
                     'iso3': country['iso3'],
                     'filename': filename
                 })
+
+    stitch_layers(country)
 
     return failures
 
@@ -210,6 +212,49 @@ def process_water_layer(country, path_in, path_out):
     return
 
 
+def stitch_layers(country):
+    """
+
+    """
+    folder = os.path.join(DATA_PROCESSED, country['iso3'], 'surface_water')
+    path_out = os.path.join(folder, 'surface_water.tif')
+
+    folder = os.path.join(DATA_PROCESSED, country['iso3'], 'surface_water')
+    paths = glob.glob(os.path.join(folder, "*.tif"))#[:5]
+
+    data = []
+    import rasterio.merge
+    for path in paths:
+        interim = rasterio.open(path)
+        data.append(interim)
+
+    bounds=None
+    res=None
+    nodata=None
+    precision=7
+
+    def merge(input1, bounds, res, nodata, precision):
+        import warnings
+        warnings.warn("Deprecated; Use rasterio.merge instead", DeprecationWarning)
+        return rasterio.merge.merge(input1, bounds, res, nodata)
+# bounds=None, res=None, nodata=None, dtype=None, precision=None
+    # dataset1 = rasterio.open("raster1.tif")
+    # dataset2 = rasterio.open("raster2.tif")
+
+    dest, output_transform=merge(data, bounds, res, nodata, precision)
+    with rasterio.open(path) as src:
+            out_meta = src.meta.copy()
+    out_meta.update({"driver": "GTiff",
+                    "height": dest.shape[1],
+                    "width": dest.shape[2],
+                    "transform": output_transform})
+    with rasterio.open(path_out, "w", **out_meta) as dest1:
+            dest1.write(dest)
+
+    return
+
+
+
 if __name__ == "__main__":
 
     os.environ['GDAL_DATA'] = ("C:\\Users\\edwar\\Anaconda3\\Library\\share\\gdal")
@@ -221,8 +266,8 @@ if __name__ == "__main__":
 
     for idx, country in countries.iterrows():
 
-        if not country['iso3'] == 'MWI': #'GHA'
-            continue
+        # if not country['iso3'] == 'RWA': #'GHA'
+        #     continue
 
         print('-Working on {}'.format(country['iso3']))
 
