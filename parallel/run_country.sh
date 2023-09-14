b2a1f4397e6cbce222a901fbaaf10edd556d49df
edwardoughton <edward.oughton@gmail.com>
2023-09-14 16:47:19 -0400

    Edit parallel

--- parallel/run_country.sh
+++ parallel/run_country.sh
@@ -3,16 +3,18 @@
 
 full_code=$1
 
-echo -----running on `hostname`: $1
+echo -----running on `hostname`: full_code
+
+iso3=${full_code:0:3}
 
 DIR=/projects/open-rigbi
 
 source activate rigbi-env
 
-mkdir -p $DIR/data/processed/$1/logs
+mkdir -p $DIR/data/processed/$iso3/logs
 
-touch $DIR/data/processed/$1/logs/$1.txt
+touch $DIR/data/processed/$iso3/logs/$iso3.txt
 
-python $DIR/scripts/process.py $1 &> $DIR/data/processed/$1/logs/$1.txt
+python $DIR/scripts/process.py $1 &> $DIR/data/processed/$iso3/logs/$1.txt
 
 echo --completed: $1
