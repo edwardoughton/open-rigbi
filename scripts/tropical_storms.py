071267d77f81affcfdaf6f8f5332767f6779be22
edwardoughton <edward.oughton@gmail.com>
2023-10-19 23:56:26 -0400

    Edit

--- scripts/tropical_storms.py
+++ scripts/tropical_storms.py
@@ -333,7 +333,7 @@ def query_tropical_storm_layers(countries, scenario):
                     coords = [(x, y)]
 
                     wind_speed = [sample[0] for sample in src.sample(coords)][0]
-                    
+                    print(x, y, src.crs, wind_speed)
                     if wind_speed > 0:
                         print(wind_speed)
 
