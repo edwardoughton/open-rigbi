b8d264d08299da1bc1c5a180989d149f1308205a
edwardoughton <edward.oughton@gmail.com>
2023-10-22 16:12:06 -0400

    Edit

--- scripts/process.py
+++ scripts/process.py
@@ -537,15 +537,15 @@ def convert_to_regional_results(country, region, scenarios, regional_level):
 
 if __name__ == "__main__":
 
-    # args = sys.argv
+    args = sys.argv
 
-    # region_id = args[1]
+    region_id = args[1]
 
-    # run_site_processing(region_id)
+    run_site_processing(region_id)
 
-    regions = get_regions({'iso3':'BGD'}, 2)#[:1]#[::-1]
+    # regions = get_regions({'iso3':'BGD'}, 2)#[:1]#[::-1]
 
-    print('Working on process_regional_flooding_layers')
-    for region in regions:
+    # print('Working on process_regional_flooding_layers')
+    # for region in regions:
 
-        run_site_processing(region['GID_2'])
+    #     run_site_processing(region['GID_2'])
