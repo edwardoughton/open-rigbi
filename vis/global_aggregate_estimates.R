fef748b54ceb19dccea5ebc8da86e88182198cde
edwardoughton <edward.oughton@gmail.com>
2023-10-22 21:19:37 -0400

    Finalize processing, vis and add fiber

--- vis/global_aggregate_estimates.R
+++ vis/global_aggregate_estimates.R
@@ -10,23 +10,23 @@ library(stringr)
 ###################
 #####Aggregate cells
 folder = dirname(rstudioapi::getSourceEditorContext()$path)
-data_directory = file.path(folder, 'results')
+data_directory = file.path(folder, 'results_flooding_final')
 setwd(data_directory)
 
 metric_files <- list.files(data_directory, pattern="inuncoast")
 
 empty_df <- data.frame(iso3=character(),
-                       iso2=character(), 
-                       country=character(), 
-                       continent=character(),
-                       radio=character(),
+                       # iso2=character(), 
+                       # country=character(), 
+                       # continent=character(),
+                       # radio=character(),
                        # network=character(),
-                       cell_count_low=numeric(),
+                       # cell_count_low=numeric(),
                        cell_count_baseline=numeric(),
-                       cell_count_high=numeric(),
-                       cost_usd_low=numeric(),
-                       cost_usd_baseline=numeric(),
-                       cost_usd_high=numeric()
+                       # cell_count_high=numeric(),
+                       # cost_usd_low=numeric(),
+                       cost_usd_baseline=numeric()#,
+                       # cost_usd_high=numeric()
                        ) 
 
 import_function = lapply(metric_files, function(x) {
@@ -38,8 +38,8 @@ import_function = lapply(metric_files, function(x) {
 data <- do.call(rbind, import_function)
 
 # data = data[(data$radio == 'LTE' | data$radio == 'UMTS'),]
-data = data[(data$radio == 'GSM'),]
-data = data[complete.cases(data[ , c('radio')]), ]
+# data = data[(data$radio == 'GSM'),]
+# data = data[complete.cases(data[ , c('radio')]), ]
 
 rm(empty_df, import_function)
 
@@ -51,9 +51,10 @@ data = data %>%
              "subsidence_model", 
              "year", 
              "returnperiod",
-             "zero",
-             "perc",
-             "percentile"), 
+             "zero"#,
+             # "perc",
+             # "percentile"
+             ), 
            sep = "_",
            convert = TRUE)
 
@@ -70,11 +71,6 @@ data$climatescenario = factor(data$climatescenario,
 data$returnperiod =  gsub(".csv", "", data$returnperiod) 
 
 #replace return periods
-# data$returnperiod =  gsub("rp00002", "rp0002", data$returnperiod)
-# data$returnperiod =  gsub("rp00005", "rp0005", data$returnperiod)
-# data$returnperiod =  gsub("rp00010", "rp0010", data$returnperiod)
-# data$returnperiod =  gsub("rp00025", "rp0025", data$returnperiod)
-# data$returnperiod =  gsub("rp00050", "rp0050", data$returnperiod)
 data$returnperiod =  gsub("rp00100", "rp0100", data$returnperiod)
 data$returnperiod =  gsub("rp00250", "rp0250", data$returnperiod)
 data$returnperiod =  gsub("rp00500", "rp0500", data$returnperiod)
@@ -82,11 +78,6 @@ data$returnperiod =  gsub("rp01000", "rp1000", data$returnperiod)
 
 #convert to probability_of_exceedance 
 data$probability = ''
-# data$probability[data$returnperiod == "rp0002"] = "50%" # (1/2) * 100 = 50%
-# data$probability[data$returnperiod == "rp0005"] = "20%" # (1/10) * 100 = 10%
-# data$probability[data$returnperiod == "rp0010"] = "10%" # (1/10) * 100 = 10%
-# data$probability[data$returnperiod == "rp0025"] = "4%" # (1/25) * 100 = 4%
-# data$probability[data$returnperiod == "rp0050"] = "2%" # (1/50) * 100 = 2%
 data$probability[data$returnperiod == "rp0100"] = "1%" # (1/100) * 100 = 1%
 data$probability[data$returnperiod == "rp0250"] = "0.4%" # (1/250) * 100 = .4%
 data$probability[data$returnperiod == "rp0500"] = "0.2%" # (1/500) * 100 = .2%
@@ -94,22 +85,12 @@ data$probability[data$returnperiod == "rp1000"] = "0.1%" # (1/1000) * 100 = .1%
 
 data$returnperiod = factor(data$returnperiod,
                           levels=c(
-                            # "rp0002",
-                            # "rp0005",
-                            # "rp0010",
-                            # "rp0025",
-                            # "rp0050",
                             "rp0100",
                             "rp0250",
                             "rp0500",
                             "rp1000"
                           ),
                           labels=c(
-                            # "1-in-2-Years",
-                            # "1-in-5-Years",
-                            # "1-in-10-Years",
-                            # "1-in-25-Years",
-                            # "1-in-50-Years",
                             "1-in-100-Years",
                             "1-in-250-Years",
                             "1-in-500-Years",
@@ -123,11 +104,6 @@ data$probability = factor(data$probability,
                              "0.2%",
                              "0.4%",
                              "1%"#,
-                             # "2%",
-                             # "4%",
-                             # "10%",
-                             # "20%",
-                             # "50%"
                            )
 )
 
@@ -136,58 +112,56 @@ data = data[data$probability == "0.1%"  |
               data$probability == "0.4%"  |
               data$probability == "1%", ] 
 
-data$radio = factor(data$radio,
-                    levels=c("GSM","UMTS",
-                             "LTE","NR"),
-                    labels=c("2G GSM","3G UMTS",
-                             "4G LTE","5G NR"
-                    )
-)
+# data$radio = factor(data$radio,
+#                     levels=c("GSM","UMTS",
+#                              "LTE","NR"),
+#                     labels=c("2G GSM","3G UMTS",
+#                              "4G LTE","5G NR"
+#                     )
+# )
+# 
+# data = data[data$subsidence_model != 'nosub', ] 
+# data$subsidence_model = NULL
+# 
+# data = data %>%
+#   mutate_at(vars(percentile), ~replace_na(., "high"))
+# data$percentile = gsub("05.csv", "low", data$percentile)
+# data$percentile = gsub("50.csv", "mean", data$percentile)
+# 
+# data = data %>%
+#   mutate_at(vars(percentile), ~replace_na(., "high"))
+# data$percentile = gsub("05.csv", "low", data$percentile)
+# data$percentile = gsub("50.csv", "mean", data$percentile)
+# 
+# # write_csv(data, 'test.csv')
+# data$percentile[data$climatescenario == 'Historical'] <- 'mean'  
 
-data = data[data$subsidence_model != 'nosub', ] 
-data$subsidence_model = NULL
-
-write_csv(data, file.path(folder, 'test.csv'))
-data = data %>%
-  mutate_at(vars(percentile), ~replace_na(., "high"))
-data$percentile = gsub("05.csv", "low", data$percentile)
-data$percentile = gsub("50.csv", "mean", data$percentile)
-
-# data$percentile2[data$climatescenario == 'Historical'] <- 'mean'  
-
-data = data %>%
-  mutate_at(vars(percentile), ~replace_na(., "high"))
-data$percentile = gsub("05.csv", "low", data$percentile)
-data$percentile = gsub("50.csv", "mean", data$percentile)
-write_csv(data, 'test.csv')
-data$percentile[data$climatescenario == 'Historical'] <- 'mean'  
-
-data$zero = NULL
-data$perc = NULL
+# data$zero = NULL
+# data$perc = NULL
 
 data_aggregated  = data %>%
-  group_by(year, climatescenario, probability, #returnperiod,
-           percentile) %>%
+  group_by(year, climatescenario, probability#, #returnperiod,
+           # percentile
+           ) %>%
   summarise(cell_count = round(sum(cell_count_baseline)/1e3,3))
 
-hist = data_aggregated[data_aggregated$climatescenario == 'Historical', ] 
+hist = data_aggregated[data_aggregated$climatescenario == 'Historical',] 
 data_aggregated = data_aggregated[data_aggregated$climatescenario != 'Historical', ] 
 hist = hist[hist$year == 'hist', ] 
 hist$year = "1980"
 data_aggregated = rbind(data_aggregated, hist)
 
-data_aggregated = spread(data_aggregated, percentile, cell_count)
-
+# data_aggregated = spread(data_aggregated, percentile, cell_count)
+data_aggregated$mean = data_aggregated$cell_count
 max_y_value = max(data_aggregated$mean)
 
-plot1 = 
-  ggplot(data_aggregated, 
+plot1 = ggplot(data_aggregated, 
                aes(x=probability, y=mean, fill=climatescenario)) + 
   geom_bar(stat="identity", position = position_dodge()) +
-  geom_errorbar(data=data_aggregated, aes(y=mean, ymin=low, ymax=high),
-                position = position_dodge(1),
-                lwd = 0.2,
-                show.legend = FALSE, width=0.7,  color="#FF0000FF") +
+  # geom_errorbar(data=data_aggregated, aes(y=mean, ymin=low, ymax=high),
+  #               position = position_dodge(1),
+  #               lwd = 0.2,
+  #               show.legend = FALSE, width=0.7,  color="#FF0000FF") +
   geom_text(aes(label = paste(round(mean,1),"")), size = 1.8,
             position = position_dodge(1), vjust =.5, hjust =-.5, angle = 90)+
   theme(legend.position = 'bottom',
@@ -195,20 +169,33 @@ plot1 =
   labs(colour=NULL,
        title = "Estimated Coastal Flooding Impact to Mobile Voice/Data Cells",
        subtitle = "Reported by Annual Probability, Year, and Climate Scenario.", 
-       x = "Annual Probability", y = "Sites (Thousands)", fill=NULL) +
+       x = "Annual Probability", y = "Cells (Thousands)", fill=NULL) +
   theme(panel.spacing = unit(0.6, "lines")) + 
   expand_limits(y=0) +
   guides(fill=guide_legend(ncol=3, title='Scenario')) +
   scale_fill_viridis_d(direction=1) +
   scale_x_discrete(expand = c(0, 0.15)) +
-  scale_y_continuous(expand = c(0, 0), limits=c(0, max_y_value+25)) +
+  scale_y_continuous(expand = c(0, 0), limits=c(0, max_y_value+40)) +
   facet_wrap(~year, ncol=4, nrow=1)
-  
-  
+# 
+# path = file.path(folder, 'figures', 'global_coastal_flooding_impacts.png')
+# ggsave(path, units="in", width=8, height=6, dpi=300)
+# 
+# data1$unit = 'sites_affected_thousands'
+# data_aggregated$unit = 'costs_usd_billions'
+# output = rbind(data1, data_aggregated)
+# output = select(output, year, climatescenario, probability, unit, low, mean, high)
+# dir.create(file.path(folder, 'report_data'), showWarnings = FALSE)
+# filename = 'coastal_flooding_assets_and_costs_3.2.csv'
+# path = file.path(folder, 'report_data', filename)
+# write.csv(output, path)
+
+data1 = data_aggregated
+
 data_aggregated  = data %>%
-  group_by(year, climatescenario, probability, #returnperiod,
-           percentile) %>%
-  summarise(cost_usd = round(sum(cost_usd_baseline)/1e9, 1))
+  group_by(year, climatescenario, probability, #returnperiod, percentile
+           ) %>%
+  summarise(cost_usd = round(sum(cost_usd_baseline)/1e9, 2))
 
 hist = data_aggregated[data_aggregated$climatescenario == 'Historical', ] 
 data_aggregated = data_aggregated[data_aggregated$climatescenario != 'Historical', ] 
@@ -216,17 +203,17 @@ hist = hist[hist$year == 'hist', ]
 hist$year = "1980"
 data_aggregated = rbind(data_aggregated, hist)
 
-data_aggregated = spread(data_aggregated, percentile, cost_usd)
-
+# data_aggregated = spread(data_aggregated, cost_usd) #percentile, 
+data_aggregated$mean = data_aggregated$cost_usd
 max_y_value = max(data_aggregated$mean)
 
 plot2 = ggplot(data_aggregated, 
                aes(x=probability, y=mean, fill=climatescenario)) + 
   geom_bar(stat="identity", position = position_dodge()) +
-  geom_errorbar(data=data_aggregated, aes(y=mean, ymin=low, ymax=high),
-                position = position_dodge(1),
-                lwd = 0.2,
-                show.legend = FALSE, width=0.7,  color="#FF0000FF") +
+  # geom_errorbar(data=data_aggregated, aes(y=mean, ymin=low, ymax=high),
+  #               position = position_dodge(1),
+  #               lwd = 0.2,
+  #               show.legend = FALSE, width=0.7,  color="#FF0000FF") +
   geom_text(aes(label = paste(round(mean,2),"Bn")), size = 1.8,
             position = position_dodge(1), vjust =.5, hjust =-.6, angle = 90)+
   theme(legend.position = 'bottom',
@@ -234,13 +221,13 @@ plot2 = ggplot(data_aggregated,
   labs(colour=NULL,
        title = "Estimated Coastal Flooding Damage Costs to Cellular Voice/Data Cells",
        subtitle = "Reported by Annual Probability, Year, and Climate Scenario.", 
-       x = "Annual Probability", y = "Damage Cost (USD Billions)", fill=NULL) +
+       x = "Annual Probability", y = "Damage Cost (USD Bn)", fill=NULL) +
   theme(panel.spacing = unit(0.6, "lines")) + 
   expand_limits(y=0) +
   guides(fill=guide_legend(ncol=3, title='Scenario')) +
   scale_fill_viridis_d(direction=1) +
   scale_x_discrete(expand = c(0, 0.15)) +
-  scale_y_continuous(expand = c(0, 0), limits=c(0, max_y_value+1.5)) +
+  scale_y_continuous(expand = c(0, 0), limits=c(0, max_y_value+2)) +
   facet_wrap(~year, ncol=4, nrow=1)
 
 ggarrange(
@@ -254,8 +241,17 @@ ggarrange(
 path = file.path(folder, 'figures', 'global_coastal_flooding_impacts.png')
 ggsave(path, units="in", width=8, height=6, dpi=300)
 
-#####################
+data1$unit = 'sites_affected_thousands'
+data_aggregated$unit = 'costs_usd_billions'
+output = rbind(data1, data_aggregated)
+output = select(output, year, climatescenario, probability, unit, mean)
+dir.create(file.path(folder, 'report_data'), showWarnings = FALSE)
+filename = 'coastal_flooding_assets_and_costs_3.2.csv'
+path = file.path(folder, 'report_data', filename)
+write.csv(output, path)
 
+
+#####################
 ###################
 #####Aggregate cells
 folder = dirname(rstudioapi::getSourceEditorContext()$path)
@@ -569,7 +565,7 @@ ggsave(path, units="in", width=8, height=6, dpi=300)
 ########################################################
 #####Tropical storms
 folder = dirname(rstudioapi::getSourceEditorContext()$path)
-data_directory = file.path(folder, 'results')
+data_directory = file.path(folder, 'results_tropical_storm')
 setwd(data_directory)
 
 metric_files <- list.files(data_directory, pattern="STORM")
@@ -579,7 +575,7 @@ empty_df <- data.frame(iso3=character(),
                        country=character(), 
                        continent=character(),
                        radio=character(),
-                       # network=character(),
+                       network=character(),
                        cell_count_low=numeric(),
                        cell_count_baseline=numeric(),
                        cell_count_high=numeric(),
@@ -730,10 +726,10 @@ data$interaction = factor(data$interaction,
                             
                           ),
 )
-
-write_csv(data, 'tropical_storm_data.csv')
-
-rm(test, historical)
+filename = 'tropical_storm_data_figure_3.3.csv'
+path_out = file.path(folder, 'report_data', filename)
+write_csv(data, path_out)
+rm(historical)
 
 df_errorbar <-
   data |>
@@ -750,15 +746,15 @@ df_errorbar <-
     mean = sum(mean),
     high = sum(high)
   )
-# max_y_value = max(data$mean)
+
 
 plot1 = ggplot(data, 
-       aes(x=interaction, y=mean, fill=continent)) + 
+               aes(x=interaction, y=mean, fill=continent)) + 
   geom_bar(stat="identity") +
   geom_errorbar(data=df_errorbar, aes(y=mean, ymin=low, ymax=high),
                 position = position_dodge(1),
                 lwd = 0.2,
-                show.legend = FALSE, width=0.7,  color="#FF0000FF") +
+                show.legend = FALSE, width=0.3,  color="#FF0000FF") +
   # geom_text(aes(label = paste(round(mean,2),"Mn")), size = 1.8,
   #           position = position_dodge(1), vjust =.5, hjust =-.5, angle = 90)+
   theme(legend.position = 'bottom',
