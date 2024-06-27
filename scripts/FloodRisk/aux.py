42d7a9acac070b91a359d2cb89656ad4cdcae65f
The Cat Collective <calculuscat@proton.me>
2024-06-27 18:32:33 -0400

    Attempt to grab data for all 10 countries

--- scripts/FloodRisk/aux.py
+++ scripts/FloodRisk/aux.py
@@ -16,6 +16,19 @@ DATA_PROCESSED: str = os.path.join(BASE_PATH, 'processed')
 EXPORTS_FOLDER: Path = DATA_FOLDER / 'exports'
 EXPORTS_FOLDER.mkdir(parents=True, exist_ok=True)
 
+MCC_TO_COUNTRY = {
+    276: 'ALB',
+    505: 'AUS',
+    302: 'CAN',
+    712: 'CRI',
+    234: 'GBR',
+    607: 'GMB',
+    639: 'KEN',
+    204: 'NLD',
+    530: 'NZL',
+    608: 'SEN'
+}
+
 class FloodRisk:
     def __init__(self, iso3):
         self.iso3 = iso3
@@ -37,12 +50,13 @@ class FloodRisk:
             chunk_count = 0
             total_chunks = math.ceil(total_rows / chunksize)
 
-            for chunk in pd.read_csv(f"{DATA_RAW}/cell_towers_2022-12-24.csv", chunksize=chunksize):
-                # Filter rows by the current MCC code
-                filtered_chunk = chunk[chunk["mcc"] == code]
-                if not filtered_chunk.empty:
-                    filtered_chunk['geometry'] = [Point(row.lon, row.lat) for row in filtered_chunk.itertuples()]
-                    gdf_chunk = gpd.GeoDataFrame(filtered_chunk, geometry='geometry')
+            for chunk in pd.read_csv(f"{DATA_RAW}/cell_towers_2022-12-24.csv", chunksize=1000):
+                #  Filter rows by the current MCC code and LTE radio type
+                filtered_chunk = chunk[chunk['mcc'] == code]
+                new_filtered_chunk = filtered_chunk[filtered_chunk['radio'] == 'LTE']
+                if not new_filtered_chunk.empty:
+                    new_filtered_chunk['geometry'] = [Point(row.lon, row.lat) for row in new_filtered_chunk.itertuples()]
+                    gdf_chunk = gpd.GeoDataFrame(new_filtered_chunk, geometry='geometry')
                     features.append(gdf_chunk)
                     chunk_count += 1
                     print(f"Processed chunk {chunk_count}/{total_chunks} for MCC {code}")
@@ -83,10 +97,10 @@ def convert_to_stations(data_dict):
     # Plotting
 def plot(lengths, radio_len):
     fig, ax = plt.subplots(figsize=(10, 6))
-    countries = [x['Country'] for x in all_new_lengths]
-    unique_stations = [x['Unique 4G Stations'] for x in all_new_lengths]
-    unique_bs_id_int = [x['Unique BS ID Int'] for x in all_new_lengths]
-    unique_sector_id_int = [x['Unique Sector ID Int'] for x in all_new_lengths]
+    countries = [x['Country'] for x in lengths]
+    unique_stations = [x['Unique 4G Stations'] for x in lengths]
+    unique_bs_id_int = [x['Unique BS ID Int'] for x in lengths]
+    unique_sector_id_int = [x['Unique Sector ID Int'] for x in lengths]
 
     bar_width = 0.25
     r1 = np.arange(len(countries))
@@ -94,37 +108,46 @@ def plot(lengths, radio_len):
     r3 = [x + bar_width for x in r2]
     r4 = [x + bar_width for x in r3]
 
-    plt.bar(r1, unique_stations, color='blue', width=bar_width, label='Unique 4G Stations')
-    plt.bar(r2, unique_bs_id_int, color='green', width=bar_width, label='Unique BS ID Int')
-    plt.bar(r3, unique_sector_id_int, color='red', width=bar_width, label='Unique Sector ID Int')
+    country_names = [MCC_TO_COUNTRY[mcc] for mcc in countries]
+
+    # Adjusted plotting values
+    plt.bar(r1, [y / 10 for y in unique_stations], color='blue', width=bar_width, label='Unique 4G Stations')
+    plt.bar(r2, [y / 10 for y in unique_bs_id_int], color='green', width=bar_width, label='Unique BS ID Int')
+    plt.bar(r3, [y / 10 for y in unique_sector_id_int], color='red', width=bar_width, label='Unique Sector ID Int')
     plt.bar(r4, radio_len, color='yellow', width=bar_width, label='Radio Data Length')
 
     plt.xlabel('Country')
     plt.ylabel('Count')
     plt.title('Comparison of Data Lengths by Country')
-    plt.xticks([r + bar_width for r in range(len(countries))], countries, rotation=45)
+    plt.xticks([r + bar_width for r in range(len(country_names))], country_names, rotation=45)
     plt.legend()
 
     plt.tight_layout()
     plt.show()
 
+
 if __name__ == "__main__":
     fr = FloodRisk('NA')
-    data_dict = fr.preprocess(276, 505, 302, 712, 234, 639, 204, 530, 608)
+    data_dict = fr.preprocess(276, 505, 302, 712, 234, 607, 639, 204, 530, 608)
     print(data_dict.keys())
     convert_to_stations(data_dict)
     
-    alb_radio = pd.read_csv(f"../data/raw/countries_data/ALB/lte_cells.csv", encoding="latin1")
-    aus_radio = pd.read_csv(f"../data/raw/countries_data/AUS/spectra_rrl/site.csv")
-    can_radio = pd.read_csv(f"../data/raw/countries_data/CAN/Site_Data_Extract.csv")
-    cri_radio = pd.read_csv(f"../data/raw/countries_data/CRI/Mobile Network Coverage and antenna characteristics_Costa Rica.csv")
-    gbr_radio = pd.read_csv(f"../data/raw/countries_data/GBR/results.csv")
-    gmb_radio = pd.read_excel(f"../data/raw/countries_data/GMB/Gambia Network_Africell.xlsx")
-    ken_radio = pd.read_csv(f"../data/raw/countries_data/KEN/all_sites.csv")
-    nld_radio = pd.read_csv(f"../data/raw/countries_data/NLD/Antennetotalen+jaaroverzicht+2023.csv")
-    nzl_radio = gpd.read_file(f"../data/raw/countries_data/NZL/cell_extract.shp")
-    sen_radio = pd.read_csv(f"../data/raw/countries_data/SEN/results.csv")
-
+    alb_radio = pd.read_csv(f"../data/raw/countries_data/ALB/lte_cells.csv", encoding='latin-1') # This is already LTE
+    aus_radio = gpd.read_file(f"../data/raw/countries_data/AUS/spectra_rrl/site.csv") # Australian data has no distinction between different -G's
+    can_radio = gpd.read_file(f"../data/raw/countries_data/CAN/sites/sites_all.shp")
+    cri_radio = pd.read_excel(f"../data/raw/countries_data/CRI/RadioBases móviles I Semestre 2020.xlsx")  
+    cri_radio = cri_radio.dropna(subset=['Unnamed: 7'])
+    cri_radio = cri_radio[cri_radio['Unnamed: 7'].str.contains('4G')]
+    gbr_radio = gpd.read_file(f"../data/raw/countries_data/GBR/sites.shp") # This data also doesn't have a distinction between different -G's
+    gmb_radio = pd.read_excel(f"../data/raw/countries_data/GMB/Gambia Network_Africell.xlsx", sheet_name='4G Cells') # This can be directly compared with OCID because it's cells
+    ken_radio = pd.read_csv(f"../data/raw/countries_data/KEN/all_sites.csv") # Kenyan data has no distinction between different -G's
+    ken_radio = ken_radio.drop_duplicates(subset=['cellName'])
+    nld_radio = pd.read_csv(f"../data/raw/countries_data/NLD/Antennetotalen+jaaroverzicht+2023.csv", skiprows=1)
+    nld_radio = nld_radio[nld_radio['Toepassing'] == 'LTE']
+    nzl_radio = gpd.read_file(f"../data/raw/countries_data/NZL/cell_extract.shp") # New Zealand data has no distinction between different -G's
+    sen_radio = pd.read_csv(f"../data/raw/countries_data/SEN/Bilan_Couverture_Orange_Dec2017.csv", encoding="latin1") # Senegalese data has no distinction between different -G's
+    sen_radio = sen_radio.drop_duplicates(subset=["Cell_ID"])
+    
     radio = [len(alb_radio), 
              len(aus_radio), 
              len(can_radio),
@@ -135,7 +158,24 @@ if __name__ == "__main__":
              len(nld_radio), 
              len(nzl_radio), 
              len(sen_radio)]
+
+    for i in all_new_lengths:
+        print(i)
+    print(radio)
     
     plot(all_new_lengths, radio)
 
-    
+    """
+    MCC: OCID - Radio
+    -----------------
+    276: 657 - 4875
+    505: 51753 - 103969
+    302: 46902 - 565472
+    712: 2391 - 3836 Close-ish?
+    234: 57611 - 144311
+    607: 5 - 176 I have no idea what happened here
+    639: 1383 - 126220
+    204: 20385 - 16388 this is okay
+    530: 4450 - 4433 this is okay
+    608:  604 - 3708 OCID is missing values here 
+    """
