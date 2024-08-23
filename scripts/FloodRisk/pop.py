44866f25c282ee79c5586c2821091f201f106028
The Cat Collective <calculuscat@proton.me>
2024-08-23 11:01:38 -0400

    Update pop.py file to use only the settlement layer file

--- scripts/FloodRisk/pop.py
+++ scripts/FloodRisk/pop.py
@@ -2,10 +2,9 @@ import geopandas as gpd
 import pandas as pd
 import rasterio
 import os
-import glob
-from constants import *
-from rasterio.mask import mask
+from tqdm import tqdm
 import json
+from rasterio.mask import mask
 
 def create_buffers(gdf, buffer_size):
     gdf['geometry'] = gdf['geometry'].apply(lambda geom: geom.buffer(buffer_size))
@@ -20,76 +19,75 @@ def process_pop(iso3, gid_level):
     settlements = rasterio.open(path_settlements, 'r+')
     settlements.nodata = 255
     settlements.crs.from_epsg(4326)
-
+    print(settlements)
     return
 
-def process_age_sex_structure(country):
+def process_settlement_layer(country):
     """
-    Clip each demographic layer to the chosen country boundary
+    Clip the settlement layer to the chosen country boundary
     and place in desired country folder.
 
     Parameters
     ----------
     country : dict
-        Contains all desired country information.
+    Contains all desired country information.
 
     """
     iso3 = country['iso3']
+    regional_level = country['gid_region']
 
-    path = os.path.join(DATA_RAW, 'settlement_layer')
-    all_paths = glob.glob(path + '/*.tif')
+    path_settlements = "/home/cisc/projects/open-rigbi/scripts/FloodRisk/data/settlement_layer/ppp_2020_1km_Aggregated.tif"
+    print(f"Processing population data for {iso3} at GID level {regional_level}")
 
-    for path in all_paths:
+    settlements = rasterio.open(path_settlements, 'r+')
+    settlements.nodata = 255
+    settlements.crs.from_epsg(4326)
 
-        # if os.path.basename(path).startswith('ppp_2020_1km_Aggregated'):
-        #     continue
+    iso3 = country['iso3']
+    # path_country = os.path.join(DATA_PROCESSED, iso3,
+    #     'national_outline.shp')
+    path_country = f"/home/cisc/projects/open-rigbi/data/processed/{iso3}/national_outline.shp"
 
-        directory_out = os.path.join(DATA_PROCESSED, iso3, 'age_sex_structure')
+    if os.path.exists(path_country):
+        country = gpd.read_file(path_country)
+    else:
+        print('Must generate national_outline.shp first' )
 
-        if not os.path.exists(directory_out):
-            os.makedirs(directory_out)
+    # path_country = os.path.join(DATA_PROCESSED, iso3)
+    path_country = f"/home/cisc/projects/open-rigbi/data/processed/{iso3}"
+    shape_path = os.path.join(path_country, 'settlements.tif')
 
-        filename = os.path.basename(path)
-        path_out = os.path.join(directory_out, filename)
+    if os.path.exists(shape_path):
+        return  print('Completed settlement layer processing')
 
-        if os.path.exists(path_out):
-            continue
+    print('----')
+    print('Working on {} level {}'.format(iso3, regional_level))
 
-        settlements = rasterio.open(path, 'r+')
-        settlements.nodata = 0
-        settlements.crs = {"init": "epsg:4326"}
+    bbox = country.envelope
+    geo = gpd.GeoDataFrame()
 
-        filename = 'national_outline.shp'
-        path_country = os.path.join(DATA_PROCESSED, iso3, filename)
+    geo = gpd.GeoDataFrame({'geometry': bbox})
 
-        if os.path.exists(path_country):
-            country = gpd.read_file(path_country)
-        else:
-            print('Must generate national_outline.shp first')
+    coords = [json.loads(geo.to_json())['features'][0]['geometry']]
 
-        geo = gpd.GeoDataFrame({'geometry': country['geometry']})
+    out_img, out_transform = mask(settlements, coords, crop=True)
 
-        coords = [json.loads(geo.to_json())['features'][0]['geometry']]
+    out_meta = settlements.meta.copy()
 
-        #chop on coords
-        out_img, out_transform = mask(settlements, coords, crop=True)
+    out_meta.update({"driver": "GTiff",
+                    "height": out_img.shape[1],
+                    "width": out_img.shape[2],
+                    "transform": out_transform,
+                    "crs": 'epsg:4326'})
 
-        # Copy the metadata
-        out_meta = settlements.meta.copy()
+    with rasterio.open(shape_path, "w", **out_meta) as dest:
+            dest.write(out_img)
 
-        out_meta.update({"driver": "GTiff",
-                        "height": out_img.shape[1],
-                        "width": out_img.shape[2],
-                        "transform": out_transform,
-                        "crs": 'epsg:4326'})
+    return print('Completed processing of settlement layer')
 
-        with rasterio.open(path_out, "w", **out_meta) as dest:
-                dest.write(out_img)
-
-    return #print('Completed processing of settlement layer')
-
-def create_union(*gdf):
-    return gdf.unary_union(*gdf)
 
 if __name__ == "__main__":
-    pass
+    df = pd.read_csv(f"/home/cisc/projects/open-rigbi/data/countries.csv", encoding='latin-1')
+    df = df[df['Exclude'] != 1]
+    country_csv = df[df['iso3'] == 'NLD']
+    process_settlement_layer(country_csv.to_records('dicts')[0])
