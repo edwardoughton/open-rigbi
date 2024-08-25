b1e78db5141481f1183bf8cb2118a3593e7f0b90
The Cat Collective <calculuscat@proton.me>
2024-08-25 18:02:27 -0400

    Update population script and add parallelization

--- scripts/FloodRisk/pop.py
+++ scripts/FloodRisk/pop.py
@@ -5,22 +5,12 @@ import os
 from tqdm import tqdm
 import json
 from rasterio.mask import mask
+from joblib import Parallel, delayed
 
 def create_buffers(gdf, buffer_size):
     gdf['geometry'] = gdf['geometry'].apply(lambda geom: geom.buffer(buffer_size))
     return gdf
     
-
-def process_pop(iso3, gid_level):
-    # path_settlements = os.path.join(DATA_RAW,'settlement_layer','ppp_2020_1km_Aggregated.tif')
-    path_settlements = "/home/cisc/projects/open-rigbi/scripts/FloodRisk/data/settlement_layer/ppp_2020_1km_Aggregated.tif"
-    print(f"Processing population data for {iso3} at GID level {gid_level}")
-
-    settlements = rasterio.open(path_settlements, 'r+')
-    settlements.nodata = 255
-    settlements.crs.from_epsg(4326)
-    print(settlements)
-    return
 
 def process_settlement_layer(country):
     """
@@ -58,7 +48,7 @@ def process_settlement_layer(country):
     shape_path = os.path.join(path_country, 'settlements.tif')
 
     if os.path.exists(shape_path):
-        return  print('Completed settlement layer processing')
+        return print('Completed settlement layer processing')
 
     print('----')
     print('Working on {} level {}'.format(iso3, regional_level))
@@ -87,7 +77,31 @@ def process_settlement_layer(country):
 
 
 if __name__ == "__main__":
-    df = pd.read_csv(f"/home/cisc/projects/open-rigbi/data/countries.csv", encoding='latin-1')
+    mobile_codes = pd.read_csv(f"../{BASE_PATH}/mobile_codes.csv", encoding='latin1')
+    mobile_codes = mobile_codes.drop_duplicates(subset=['mcc'])
+    df = pd.read_csv(f"../{BASE_PATH}/countries.csv", encoding='latin-1')
     df = df[df['Exclude'] != 1]
-    country_csv = df[df['iso3'] == 'NLD']
-    process_settlement_layer(country_csv.to_records('dicts')[0])
+    print("Loaded data")
+    
+    error = []
+    def process_country(country_code, country_csv):
+        country_regions = []
+        try:
+            level = df.loc[df['iso3'] == country_code, 'gid_region'].values[0]
+            level = int(level)
+            country_code_2 = df[df['iso3'] == country_code]['iso2'].values[0]
+            mcc = mobile_codes[mobile_codes['iso3'] == country_code]['mcc'].values[0]
+            # print(f"Processing Country code: {country_code}")
+            country = country_csv[country_csv['iso3'] == country_code]
+            country = country.to_records('dicts')[0]
+            print(f"Processing Country code: {country_code}")
+            process_settlement_layer(country)
+
+        except Exception as e:
+            print(f"Error processing country code {country_code}: {e}")
+            error.append(country_code)
+            return country_code, pd.DataFrame()
+        
+    results = Parallel(n_jobs=-1)(delayed(process_country)(country_code, df) for country_code in df['iso3'])
+    process_settlement_layer(country)
+    
