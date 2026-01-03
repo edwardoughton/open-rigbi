a964487983b08db2c658362063cc47d762d9579f
edward.oughton <edward.oughton@gmail.com>
2026-01-03 11:39:16 -0500

    Delete old scripts

--- scripts/FloodRisk/constants.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-BASE_PATH: Path = Path('./data')
-DATA_FOLDER: Path = Path('./data')
-DATA_RAW: Path = Path(BASE_PATH / 'raw')
-DATA_INTERMEDIATE: Path = Path(BASE_PATH / 'intermediate')
-DATA_PROCESSED: Path = Path(BASE_PATH / 'processed')
-EXPORTS_FOLDER: Path = Path(DATA_FOLDER / 'exports')
-EXPORTS_FOLDER.mkdir(parents=True, exist_ok=True)
